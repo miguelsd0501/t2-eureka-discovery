039c30c4ab89801437d1bc6c59d1de59dad6769b
Miguel Angel Santiago <miguelsd0501@gmail.com>
2019-09-08 01:12:15 -0500

    discovery for course udemy

--- src/main/java/com/sd/ms/discovery/DiscoveryApplication.java
+++ src/main/java/com/sd/ms/discovery/DiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.t2.microservice.eureka;
+package com.sd.ms.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class T2MicroserviceEurekaApplication {
+public class DiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(T2MicroserviceEurekaApplication.class, args);
+		SpringApplication.run(DiscoveryApplication.class, args);
 	}
 
 }
